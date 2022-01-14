76c67b6179f41104fd46689e4dbfe20812835228
kamil <masacz88@gmail.com>
2022-01-14 19:39:56 +0100

    Cw.2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,17 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        Student[] studenci = new Student[3];
+        Student student = new Student ("Jan","Nowak",76897,false);
+        studenci[0] = student;
+        student = new Student ("Jan", "Kowalski", 54689, true);
+        studenci[1] = student;
+        student = new Student ("Edward","Ącki",11990,true);
+        studenci[2] = student;
+
+        for (Student wypiszStudentów : studenci){
+            student.wypisanieDanych();
+            System.out.println("");
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    public String imię;
+    public String nazwisko;
+    public int numerAlbumu;
+    public boolean statusStudenta;
+
+    public Student(String imię, String nazwisko, int numerAlbumu, boolean statusStudenta) {
+        this.imię = imię;
+        this.nazwisko = nazwisko;
+        this.numerAlbumu = numerAlbumu;
+        this.statusStudenta = statusStudenta;
+    }
+
+    public void wypisanieDanych() {
+        System.out.println("Imię: " + this.imię);
+        System.out.println("Nazwisko: " + this.nazwisko);
+        System.out.println("Numer Albumu: " + this.numerAlbumu);
+        if (false == statusStudenta) {
+            System.out.println("Status Studenta: Nieaktywny");
+        } else {
+            System.out.println("Status Studenta: Aktywny");
+        }
+    }
+}
